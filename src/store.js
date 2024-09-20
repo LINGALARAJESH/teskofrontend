1910e9aae1fd34bac02556ace50f1db6c1c6fc85
LINGALARAJESH <rajeshlingala08@gmail.com>
2024-09-20 20:59:58 +0530

    heeehes

--- src/store.js
+++ src/store.js
@@ -1,36 +1,41 @@
-import { createStore,combineReducers, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
-import {composeWithDevTools} from "redux-devtools-extension"
-import {productListReducers} from "./reducers/productReducers"
-import { productDetailsReducers } from './reducers/productReducers';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducers, productDetailsReducers } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import {userLoginReducers} from "./reducers/userReducers"
-import {userRegisterReducers} from "./reducers/userReducers"
-import {OrderItemReducer,ShippingReducer,OrderReducer} from './reducers/cartReducers';
+import { userLoginReducers, userRegisterReducers } from './reducers/userReducers';
+import { OrderItemReducer, ShippingReducer, OrderReducer } from './reducers/cartReducers';
 
+// Combine reducers
+const reducer = combineReducers({
+  productList: productListReducers,
+  productDetails: productDetailsReducers,
+  cart: cartReducer,
+  userLogin: userLoginReducers,
+  userRegister: userRegisterReducers,
+  orderdata: OrderReducer,
+  shippingdata: ShippingReducer,
+  orderitemdata: OrderItemReducer,
+});
 
-const reducer=combineReducers({
-    productList:productListReducers,
-    productDetails:productDetailsReducers,
-    cart:cartReducer,
-    userLogin:userLoginReducers,
-    userRegister:userRegisterReducers,
-    orderdata:OrderReducer,
-    shippingdata:ShippingReducer,
-    orderitemdata:OrderItemReducer,
-})
+// Fetch user info from local storage
+const userInfoFromStorage = localStorage.getItem('userInfo') 
+  ? JSON.parse(localStorage.getItem('userInfo')) 
+  : null;
 
-const userInfoFromStorage=localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')):null
+// Define the initial state
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
-const initialState={
-  userLogin:{userInfo:userInfoFromStorage}
-}
-const middleware=[thunk]
+// Set up middleware
+const middleware = [thunk];
 
+// Create the store with reducer, initial state, and middleware
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-  );
-  
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
 export default store;
